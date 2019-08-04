3e1da3a18cd12a05279f5b60586fffff487fe25c
Yunus Chang <yunus.chang@gmail.com>
2019-08-04 15:38:16 -0700

    Added new information to the head buttons

--- medical_skills/MS_script.js
+++ medical_skills/MS_script.js
@@ -27,7 +27,30 @@ class info
 	}
 }
 
-var information = ["",
+var soreThroatQuestions = ["",
+"Where does it hurt?", 
+"When did it start?",
+"Tell me how it began?",
+"What were you doing when it began?",
+"Have you been around anyone else recently? Have you been around anyone with strep throat?",
+"Have you had a cough?",
+"Have you had a fever and/or chills?",
+"Has your voice changed?",
+"Can you swallow OK?",
+"How’s your appetite?", 
+"How’s your energy level?",
+"What’s happened since it started?",
+"What’s going on now?",
+"Relevant social history", "/Have you been around anyone ill with similar symptoms?", "/Have you had oral sex recently?", "/Do you smoke?",
+"Relevant ROS:",
+"Pulmonary", "/Do you have a cough?",
+"Gastrointestinal" , "/Difficulty swallowing?", "/Pain with swallowing?", "/Do you experience heartburn?", "/Do you experience a bad taste in your mouth, perhaps at night?",
+"Genitourinary(Ob/Gyn)", "/Pain with urination?", "/Vaginal discharge?", "/New sexual partner?",
+"Neurologic", "/Have you had a headache?", "/Have you had neck pain?",
+"Skin", "/Have you had a rash?"
+]
+
+var chiefComplaint2Questions = ["",
 "What are the common risk factors?",
 "How was the patient's health before incidence?",
 "Onset, duration, and frequency of problem?",
@@ -35,34 +58,20 @@ var information = ["",
 "Symptoms and severity: Scale from 1-10, continuous or intermittent",
 "Quality of pain?",
 "What makes it worse and what makes it better?",
-"What are the associated symptoms?",
+"What are the associated symptoms?"]
+
+var genQuestions = [
 "Past medical history", "/Childhood illnesses", "/Adult illnesses", "/Accidents & Injuries",
 "Past surgical history" ,"/Hospitalizations", "/Surgeries",
 "Medications",
 "Allergies/Immunizations",
-"Social History", "/Smoking History", "/Alcohol History", "/Recreational drug use", "/Sexual History", "/Travel history", "/Employment history" ,
+"Social history", "/Smoking history", "/Alcohol history", "/Recreational drug use", "/Sexual history", "/Travel history", "/Employment history" ,
 "Family History",
 "Review of systems", "/Consitutional", "/HEENT" ,"/Heme/Lymph","/Pulmonary","/Cardiac","/Endocrine","/Gastrointestinal" ,"/Genutourinary(Ob/Gyn)","/Musculoskeletal" ,"/Neurologic","/Skin","/Psychiatric"];
 
 var i = 0;
 var j = -1;
 var questions = [];
-while(i < information.length)
-{
-	if(information[i][0] == '/')
-	{
-		var length = information[i].length;
-		var string = information[i].substring(1,length);
-		questions[j].addHint(string);
-	}
-	else
-	{
-	let x = new info(information[i]);
-	questions.push(x);
-	j++;
-	}
-	i++;
-}
 
 function prev()
 {
@@ -186,5 +195,29 @@ function CCfunc()
   document.getElementById("CCButton").remove();
   document.getElementById("CC").innerHTML = "Chief Complaint: " + CC;
   document.getElementById("question").innerHTML = "";
+  var information = [];
+  if(CC == "Sore Throat")
+  	information = soreThroatQuestions;
+  if(CC == "Chief Complaint 2")
+  	information = chiefComplaint2Questions;
+  if(CC == "Chief Complaint 3")
+  	information = [""];
+  information = information.concat(genQuestions);
+  while(i < information.length)
+  {
+	if(information[i][0] == '/')
+	{
+		var length = information[i].length;
+		var string = information[i].substring(1,length);
+		questions[j].addHint(string);
+	}
+	else
+	{
+	let x = new info(information[i]);
+	questions.push(x);
+	j++;
+	}
+	i++;
+   }
   CCchosen = 1;
 }
--- medical_skills/PE_script.js
+++ medical_skills/PE_script.js
@@ -1,8 +1,39 @@
 
-var critical = [];
-var locale = [];
-var neckCritical = ["Mass presence", "Absess" , "Redness", "Discharge"];
-var neckLocale = ["Upper" ,  "Middle", "Lower"];
+var inspect = [];
+var test = [];
+
+var headInspect = ["Scalp","Hair","Skull","Face"];
+var headTest = [];
+
+var eyesInspect = ["Sclera and conjectiva","Cornea and lens"];
+var eyesTest = ["Pupillary reaction to light",
+"Test for convergence",
+"Assess extracular muscles/Six cardinal directions of gaze",
+"Opthalmoscopic exam(Papilledema,Inspection of retina,red reflex)",
+"Assess visual acuity",
+"Static finger wiggle test" ,
+"Kinetic red target test",
+"Check for nystagmus"];
+
+var earsInspect = ["Auricle" , "Ear canal and drum"];
+var earsTest = [ "Auditory acuity", "Whispered voice test", "Tuning fork test (Weber Test, Rinne Test)"];
+
+var noseInspect = ["Check anterior and inferior surfaces", "Check nasal vestibule and passages", "Examine the nasal mucosa"];
+var noseTest = ["Nasal obstruction", "Palpate for sinus tenderness (Frontal sinuses Maxillary sinuses)"];
+
+var mouthInspect = ["Mucosa, dry?",
+"Check mucosa for ulcers",
+"Look for erythema or discharge in back of throat",
+"Check teeth for decay", 
+"Symmetrical uvula",
+"Tongue Deviation, ask to stick out tongue and move from side to side"];
+var mouthTest = [];
+
+var neckInspect = ["Check for deviation of trachea",
+"Ask patient to swallow and check for upward and downward movement of crico-thyroid cartilage (more observable in males)",
+"Check Lymph nodes, pre-auricular, post-auricular, occipital, mandibular, submental, cervical chain, supraclavicular"];
+var neckTest = [];
+
 var checkOutput = 0;
 var bodyPart; //used to change body part header
 
@@ -11,35 +42,35 @@ function output()
 	var i = 0;
 	var ul0 = document.createElement("ul");
 	ul0.setAttribute("id","symptoms");
-	while(i < critical.length)
+	while(i < inspect.length)
 	{
 		var x = document.createElement("li");
-		x.appendChild(document.createTextNode(critical[i]));
+		x.appendChild(document.createTextNode(inspect[i]));
 		ul0.appendChild(x);
 		i++;
 	}
 	i = 0;
 	var ul1 = document.createElement("ul");
-	ul1.setAttribute("id","locale");
-	while(i < locale.length)
+	ul1.setAttribute("id","test");
+	while(i < test.length)
 	{
 		var x = document.createElement("li");
-		x.appendChild(document.createTextNode(locale[i]));
+		x.appendChild(document.createTextNode(test[i]));
 		ul1.appendChild(x);
 		i++;
 	}
 	var h0 = document.createElement("p");
 	h0.innerHTML = "Inspection:";
-	//var h1 = document.createElement("p");
-	//h1.innerHTML = "Locations:";
+	var h1 = document.createElement("p")
+	h1.innerHTML = "Test:";
 	var a = document.createElement("a");
 	var b = document.getElementById("pointersHeader");
 	a.setAttribute("id","examPointers")
 	b.appendChild(a);
 	a.appendChild(h0);
 	a.appendChild(ul0);
-	//a.appendChild(h1);
-	//a.appendChild(ul1);
+	a.appendChild(h1);
+	a.appendChild(ul1);
 	var header = document.getElementById("bodyPart");
 	header.innerHTML = bodyPart;
 }
@@ -61,8 +92,8 @@ function resetExamPoints()
 	var x = document.getElementById("examPointers")
 	if(x != null)
 		x.remove();
-	critical = [];
-	locale = [];
+	inspect = [];
+	test = [];
 }
 
 function headfunc()
@@ -74,13 +105,15 @@ function headfunc()
 	y.setAttribute("width","423");
 	y.setAttribute("height","396");
 	var x = document.getElementById("bodyPart");
-	x.innerHTML = "Head/Please Choose a Body Part";
+	x.innerHTML = "Please Choose a Body Part";
 }
 
 function headTop()
 {
 	resetExamPoints();
 	bodyPart = "Head";
+	inspect = headInspect;
+	test = headTest;
 	output();
 }
 
@@ -88,6 +121,8 @@ function ears()
 {
 	resetExamPoints();
 	bodyPart = "Ears";
+	inspect = earsInspect;
+	test = earsTest;
 	output();
 }
 
@@ -95,13 +130,15 @@ function eyes()
 {
 	resetExamPoints();
 	bodyPart = "Eyes";
+	inspect = eyesInspect;
+	test = eyesTest;
 	output();
 }
 function neck()
 {
 	resetExamPoints();
-	critical = neckCritical;
-	locale = neckLocale;
+	inspect = neckInspect;
+	test = neckTest;
 	bodyPart = "Neck";
 	output();
 }
@@ -110,6 +147,8 @@ function mouth()
 {
 	resetExamPoints();
 	bodyPart = "Mouth";
+	inspect = mouthInspect;
+	test = mouthTest;
 	output();
 }
 
@@ -117,6 +156,8 @@ function nose()
 {
 	resetExamPoints();
 	bodyPart = "Nose";
+	inspect = noseInspect;
+	test = noseTest;
 	output();
 }
  
